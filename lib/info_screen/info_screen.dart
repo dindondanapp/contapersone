a5c79b21b5639009324e9f28c89e9347ad8cb762
Alessandro Diano <alessandro.diano@me.com>
2020-12-04 13:47:50 +0100

    fix: InfoScreen text and alignment on large screens

--- lib/info_screen/info_screen.dart
+++ lib/info_screen/info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 /// A screen with basic information on this app
 class InfoScreen extends StatelessWidget {
@@ -8,31 +9,42 @@ class InfoScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('Informazioni'),
       ),
-      body: ConstrainedBox(
-        constraints: BoxConstraints(maxWidth: 400),
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              SizedBox(height: 20),
-              Center(
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 150),
-                  child: Image.asset(
-                    'assets/round_icon.png',
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 400),
+            child: Column(
+              children: [
+                SizedBox(height: 20),
+                Center(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 150),
+                    child: Image.asset(
+                      'assets/round_icon.png',
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 40),
-              Text(
-                '''Contapersone è lo strumento semplice ed efficace per contare gli accessi a eventi, esercizi commerciali, celebrazioni religiose e tanto altro, anche i presenza di ingressi multipli.
+                SizedBox(height: 40),
+                Text(
+                  '''Contapersone è lo strumento semplice ed efficace per contare gli accessi a eventi, esercizi commerciali, celebrazioni religiose e tanto altro, anche i presenza di ingressi multipli.
 
 Per iniziare crea un nuovo contapersone, specificando facoltativamente la capienza. Potrai poi condividere il conteggio con altri dispositivi, ad esempio per monitorare più ingressi contemporaneamente, tramite un semplice link o un codice QR. Avrai sempre sotto controllo il conteggio totale di tutti i dispositivi, sincronizzato in tempo reale.
 
-Il servizio è gratuito per tutti, ma nasce in particolare per il monitoraggio degli accessi in chiesa. Se sei responsabile di una parrocchia puoi accedere all'"Area riservata parrocchie", per avere accesso alle statistiche di utilizzo e ad altri utili servizi sul portale web di DinDonDan!''',
-                textAlign: TextAlign.justify,
-              )
-            ],
+Il servizio nasce in particolare per il monitoraggio degli accessi in chiesa, ma è ora disponibile liberamente per tutti. Se sei responsabile di una parrocchia puoi accedere all'"Area riservata parrocchie", per avere accesso alle statistiche di utilizzo e ad altri utili servizi sul portale web di DinDonDan.
+
+Contapersone è un progetto open source offerto gratuitamente dall'associazione DinDonDan App.''',
+                  textAlign: TextAlign.justify,
+                ),
+                SizedBox(height: 40),
+                RaisedButton(
+                  child: Text('Sostienici con una donazione'),
+                  onPressed: () => launch('https://dindondan.app/donate.php'),
+                  textTheme: ButtonTextTheme.primary,
+                  color: Theme.of(context).primaryColor,
+                )
+              ],
+            ),
           ),
         ),
       ),
