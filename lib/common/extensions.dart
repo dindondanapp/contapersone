f5be2e2925f6e2a870ac512e45e7bc4bb0e6be58
Alessandro Diano <alessandro.diano@me.com>
2020-12-08 10:35:40 +0100

    doc: add comments to extensions

--- lib/common/extensions.dart
+++ lib/common/extensions.dart
@@ -5,10 +5,8 @@ import 'package:flutter/material.dart';
 // This file contains miscellaneous extensions
 
 extension ColorToMaterialColor on Color {
-  /// Converts the color into a [MaterialColor], creating variations based on brightness
+  /// Convert the color into a [MaterialColor], creating variations based on lightness
   MaterialColor toMaterialColor() {
-    final base = HSLColor.fromColor(this);
-
     final lightnessMap = {
       050: 1.8,
       100: 1.6,
@@ -27,19 +25,25 @@ extension ColorToMaterialColor on Color {
       lightnessMap.map<int, Color>(
         (key, multiplier) => MapEntry(
           key,
-          base
-              .withLightness((base.lightness * multiplier)
-                  .sat(lower: 0, upper: 1)
-                  .toDouble())
-              .toColor(),
+          _lightnessVariation(multiplier),
         ),
       ),
     );
   }
+
+  /// Create a new color with the given lightness [multiplier]
+  Color _lightnessVariation(num multiplier) {
+    final base = HSLColor.fromColor(this);
+    return base
+        .withLightness(
+          (base.lightness * multiplier).sat(lower: 0, upper: 1).toDouble(),
+        )
+        .toColor();
+  }
 }
 
 extension Saturation on num {
-  /// Saturates the number between given [lower] and [upper] bounds, if provided.
+  /// Saturate the number between given [lower] and [upper] bounds, if provided.
   num sat({num lower, num upper}) {
     return min(upper ?? double.infinity, max(lower ?? -double.infinity, this));
   }
