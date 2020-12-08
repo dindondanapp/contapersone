98c7da5f602333aef30a094fc43d800f6f731654
Alessandro Diano <alessandro.diano@me.com>
2020-12-08 10:28:12 +0100

    doc: add comments for CounterToken

--- lib/common/entities.dart
+++ lib/common/entities.dart
@@ -1,20 +1,27 @@
-import 'dart:convert';
 import 'dart:math';
 
-// This file containes miscellaneous entities
+// This file containes miscellaneous entities that are needed across multiple screens
 
+/// Unique identifier for a shared counter in the database
 class CounterToken {
   final String _value;
 
-  CounterToken([int length = 32]) : _value = _generateToken();
-  CounterToken.fromString(String value) : _value = value;
+  /// Create a new shared counter identifier, represented by a 32 characters
+  /// hex [String]
+  CounterToken() : _value = _generateToken(32);
+
+  /// Create a counter identifier from its given [String] representation
+  // TODO: add format check (but be careful with cross-version sharing)
+  CounterToken.fromString(this._value);
 
   static String _generateToken([int length = 32]) {
-    final values =
-        List<int>.generate(length, (i) => Random.secure().nextInt(256));
-    return base64Url.encode(values);
+    final random = Random.secure();
+    return List<String>.generate(
+        length, (i) => random.nextInt(16).toRadixString(16)).join();
   }
 
+  /// Return a string representation of the shared counter identifier.
+  /// The output can be parsed with the [fromString] constructor.
   String toString() {
     return _value;
   }
